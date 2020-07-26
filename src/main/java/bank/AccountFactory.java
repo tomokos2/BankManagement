b94bff7bb7aa65ea09d6dca751dc1dff6234e055
tomokos2 <tomokos2@illinois.edu>
2020-07-25 19:05:51 -0500

    Also migrated accountfactory to datetime

--- src/main/java/bank/AccountFactory.java
+++ src/main/java/bank/AccountFactory.java
@@ -5,11 +5,11 @@ import main.java.bank.accountTypes.CheckingAccount;
 import main.java.bank.accountTypes.RetirementAccount;
 import main.java.bank.accountTypes.SavingsAccount;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 
 public abstract class AccountFactory {
-    public static Account makeAccount(String type, int balance, Date dateCreated, LinkedList<Transaction> transactions) {
+    public static Account makeAccount(String type, int balance, LocalDateTime dateCreated, LinkedList<Transaction> transactions) {
         switch (type) {
             case "cd":
                 return new CDAccount(balance, dateCreated, transactions);
