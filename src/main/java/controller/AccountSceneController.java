baaa6521ea3b00387c8612325a6762fe5d0ffdbf
tomokos2 <tomokos2@illinois.edu>
2020-07-28 20:11:56 -0500

    Display transactions in scrollview

--- src/main/java/controller/AccountSceneController.java
+++ src/main/java/controller/AccountSceneController.java
@@ -7,13 +7,20 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import main.java.bank.Account;
 import main.java.bank.Client;
+import main.java.bank.Transaction;
 import main.java.services.BankDatabase;
+
+import java.beans.EventHandler;
+import java.util.LinkedList;
 
 
 public class AccountSceneController {
 
     @FXML
     VBox accountScrollContent;
+
+    @FXML
+    VBox transactionScrollContent;
 
     @FXML
     ToggleGroup accountType;
@@ -28,6 +35,16 @@ public class AccountSceneController {
                 for (Account a : BankDatabase.getAccounts(Integer.toString(Client.getCurrentClient().getId()), rb.getId())) {
                     Button accountButton = new Button();
                     accountButton.setText("Account Number: " + a.getId());
+                    accountButton.setOnAction(e -> {
+                        LinkedList<Transaction> transactions = a.getTransactions();
+                        for (Transaction t : transactions) {
+                            Label tLabel = new Label();
+                            tLabel.setText("Amount: " + t.getAmount() + ", Date: " + t.getDate().toString() +
+                                    ", Status: " + t.getStatusString() + ", Type: " + t.getType());
+                            transactionScrollContent.getChildren().add(tLabel);
+
+                        }
+                    });
                     // Set event handler
                     accountScrollContent.getChildren().add(accountButton);
                 }
