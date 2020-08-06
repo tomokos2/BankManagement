7d2dc67b34ab2f7eda210b603a3d940d8a658b8e
tomokos2 <tomokos2@illinois.edu>
2020-08-06 06:34:33 -0500

    Added admin files

---
+++ src/main/java/controller/AdminMainController.java
@@ -0,0 +1,32 @@
+package main.java.controller;
+
+import javafx.fxml.FXML;
+import main.java.bank.Client;
+import main.java.services.Navigator;
+
+public class AdminMainController {
+
+    public void loginAdmin(Client c) {
+
+    }
+
+    @FXML
+    private void onAddClientClick() {
+
+    }
+
+    @FXML
+    private void onDeleteClientClick() {
+
+    }
+
+    @FXML
+    private void onEditClientClick() {
+
+    }
+
+    @FXML
+    private void onLogout() {
+        Navigator.navigate("welcomeScene.fxml");
+    }
+}
